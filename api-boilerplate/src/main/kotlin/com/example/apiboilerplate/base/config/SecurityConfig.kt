00ef581ce12c3452ca4684cfeb3238eb5bbfbb0b
Rodrigo <rodrigo.hilkner@gmail.com>
2022-02-04 15:18:03 -0300

    Add BCrypt password encoding

---
+++ api-boilerplate/src/main/kotlin/com/example/apiboilerplate/base/config/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package com.example.apiboilerplate.base.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun encoder(): PasswordEncoder? {
+        return BCryptPasswordEncoder()
+    }
+
+}
