a194dbf0538ca003aae20c79f03c9b778e39bb79
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2022-03-29 15:04:42 -0300

    Remove unused classes

--- main-api/src/main/kotlin/com/example/apiboilerplate/repositories/CustomPlateRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.apiboilerplate.repositories
-
-import com.example.apiboilerplate.models.CustomPlate
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CustomPlateRepository: JpaRepository<CustomPlate, Long>
--- main-api/src/main/kotlin/com/example/apiboilerplate/repositories/address/MasterAddressRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.apiboilerplate.repositories.address
-
-import com.example.apiboilerplate.models.address.MasterAddress
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MasterAddressRepository: JpaRepository<MasterAddress, Long>
